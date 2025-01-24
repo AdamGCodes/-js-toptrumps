2d969ca4a0352afda9ddcf43815a0047be17d38d
Archietheowl <adsg1206@gmail.com>
2025-01-24 22:47:07 +0000

    experimenting with using a modeal instead of message box area. Have rolled back for now and will revisit.

--- js/app.js
+++ js/app.js
@@ -116,15 +116,7 @@ const init = () => {
 }
 init()
 
-const handleHandMessage = () => {
-    let person = prompt("Please enter your name", "Harry Potter");
-    let text;
-    if (person == null || person == "") {
-        text = "User cancelled the prompt.";
-    } else {
-        text = "Hello " + person + "! How are you today?";
-    }
-}
+
 //+++++++++++++++++++++++++++++++++++++ Start Game +++++++++++++++++++++++++++++++++++++
 
 const shuffle = (currentDeck) => {
